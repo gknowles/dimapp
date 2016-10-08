3269ad886ecb2676fbad3fab818efaad3b30c992
gknowles <gknowles@ieee.org>
2016-10-08 00:24:27 -0700

    Move EX_* definitions to compiler specific include

--- include/dim/app.h
+++ include/dim/app.h
@@ -24,31 +24,11 @@ public:
 // returns exit code
 int appRun(ITaskNotify & app);
 
-// program exit codes
-#ifndef EX_OK
+// additional program exit codes
 enum {
-    EX_OK = 0, // success
-
-    // 1-2 available for application
-
-    EX_ABORTED = 3, // assert() failure or direct call to abort()
-
-    // 4-63 available for application
-
-    // 64-78 are borrowed from unix's sysexits.h
-    EX_USAGE = 64,       // bad command line
-    EX_DATAERR = 65,     // bad input file
-    EX_UNAVAILABLE = 69, // failed for some other reason
-    EX_SOFTWARE = 70,    // internal software error
-    EX_OSERR = 71,       // some os operation failed
-    EX_IOERR = 74,       // console or file read/write error
-    EX_NOPERM = 77,      // insufficient permission
-    EX_CONFIG = 78,      // configuration error
-
     // 100+ reserved for application
     EX__APPBASE = 100,
 };
-#endif
 
 void appSignalShutdown(int exitcode = EX_OK);
 
--- include/dim/compiler/visualc.h
+++ include/dim/compiler/visualc.h
@@ -16,3 +16,22 @@
 
 #define _NO_LOCALES 0
 #define _ITERATOR_DEBUG_LEVEL 0
+
+// program exit codes
+// On *nix the EX_* constants are defined in <sysexits.h>
+#ifndef EX_OK
+enum {
+    EX_OK = 0,
+    EX_USAGE = 64,       // bad command line
+    EX_DATAERR = 65,     // bad input file
+    EX_UNAVAILABLE = 69, // failed for some other reason
+    EX_SOFTWARE = 70,    // internal software error
+    EX_OSERR = 71,       // some os operation failed
+    EX_IOERR = 74,       // console or file read/write error
+    EX_NOPERM = 77,      // insufficient permission
+    EX_CONFIG = 78,      // configuration error
+
+    // Microsoft specific
+    EX_ABORTED = 3, // assert() failure or direct call to abort()
+};
+#endif
