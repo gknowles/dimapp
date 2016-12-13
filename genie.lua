24e78907306fb33f1b5991d335b1384849ca86ab
gknowles <gknowles@ieee.org>
2016-12-13 11:47:55 -0800

    Add boten-1.11.34 vendor files

--- genie.lua
+++ genie.lua
@@ -12,13 +12,13 @@ solution "dimapp"
   }
   includedirs { 
     "include",
-    "vendor/sodium/include" 
+    "vendor/sodium/include",
+    "vendor/botan-1.11/include"
   }
   links { 
     "vendor/sodium/$(Platform)/$(Configuration)/$(PlatformToolset)/static/libsodium" 
   }
   targetdir "bin"
-  pchheader "pch.h"
   debugdir "$(TargetDir)"
   buildoptions { "/std:c++latest" }
 
@@ -44,6 +44,7 @@ project "dimapp"
     "include/**",
     "src/*"
   }
+  pchheader "pch.h"
   pchsource "src/pch.cpp"
 
 project "dimapp-cli"
@@ -53,6 +54,7 @@ project "dimapp-cli"
   files { 
     "src/cli/*"
   }
+  pchheader "pch.h"
   pchsource "src/cli/pch.cpp"
 
 project "dimapp-win8"
@@ -62,6 +64,7 @@ project "dimapp-win8"
   files { 
     "src/win8/*"
   }
+  pchheader "pch.h"
   pchsource "src/win8/pch.cpp"
 
 project "dimapp-xml"
@@ -71,7 +74,34 @@ project "dimapp-xml"
   files { 
     "src/xml/*"
   }
+  pchheader "pch.h"
   pchsource "src/xml/pch.cpp"
+
+project "vendor-botan"
+  kind "StaticLib"
+  location "projects"
+  vpaths { 
+    ["*"] = { "vendor/botan-1.11/include", "vendor/botan-1.11/src" } 
+  }
+  files {
+    "vendor/botan-1.11/include/*",
+    "vendor/botan-1.11/src/*"
+  }
+  buildoptions { 
+    "/bigobj",
+    "/wd4100",
+    "/wd4101",
+    "/wd4127",
+    "/wd4146",
+    "/wd4250", 
+    "/wd4244",
+    "/wd4245",
+    "/wd4267", 
+    "/wd4334",
+    "/wd4702",
+    "/wd4800",
+    "/wd4996"
+  }
 
 group "-meta"
 project "-meta"
@@ -95,6 +125,7 @@ project "hpack"
   location "projects"
   vpaths { ["*"] = "tests/hpack" }
   files { "tests/hpack/**" }
+  pchheader "pch.h"
   pchsource "tests/hpack/pch.cpp"
   links { "dimapp", "dimapp-win8", "dimapp-cli" }
 
@@ -103,6 +134,7 @@ project "http"
   location "projects"
   vpaths { ["*"] = "tests/http" }
   files { "tests/http/**" }
+  pchheader "pch.h"
   pchsource "tests/http/pch.cpp"
   links { "dimapp", "dimapp-win8", "dimapp-cli" }
 
@@ -111,6 +143,7 @@ project "tls"
   location "projects"
   vpaths { ["*"] = "tests/tls" }
   files { "tests/tls/**" }
+  pchheader "pch.h"
   pchsource "tests/tls/pch.cpp"
   links { "dimapp", "dimapp-win8", "dimapp-cli" }
 
@@ -119,6 +152,7 @@ project "xml"
   location "projects"
   vpaths { ["*"] = "tests/xml" }
   files { "tests/xml/**" }
+  pchheader "pch.h"
   pchsource "tests/xml/pch.cpp"
   links { "dimapp", "dimapp-win8", "dimapp-cli", "dimapp-xml" }
 
@@ -130,6 +164,7 @@ project "pargen"
   location "projects"
   vpaths { ["*"] = "tools/pargen" }
   files { "tools/pargen/**" }
+  pchheader "pch.h"
   pchsource "tools/pargen/pch.cpp"
   links { "dimapp", "dimapp-win8", "dimapp-cli" }
 
