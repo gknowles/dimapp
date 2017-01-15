23b7665f6a5d3b6ecaaf544f81ce5aa1c368f912
gknowles <gknowles@ieee.org>
2017-01-14 23:04:27 -0800

    Tweak comments

--- tests/http/httptest.cpp
+++ tests/http/httptest.cpp
@@ -222,6 +222,7 @@ void Application::onTask() {
 *
 ***/
 
+//===========================================================================
 int main(int argc, char * argv[]) {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     _set_error_mode(_OUT_TO_MSGBOX);
