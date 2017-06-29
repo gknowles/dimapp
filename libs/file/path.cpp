b0f192bcef72f94a3b3569686adbc45fcaf37c5b
gknowles <gknowles@ieee.org>
2017-06-29 09:55:59 -0700

    WIP full featured Path class

--- libs/file/path.cpp
+++ libs/file/path.cpp
@@ -12,10 +12,22 @@ namespace fs = std::experimental::filesystem;
 
 /****************************************************************************
 *
-*   Variables
+*   Declarations
 *
 ***/
 
+namespace {
+
+struct Count {
+    unsigned m_rootLen;
+    unsigned m_dirLen;
+    unsigned m_stemLen;
+    unsigned m_extLen;
+
+    Count(string_view path);
+};
+
+} // namespace
 
 
 /****************************************************************************
@@ -35,6 +47,115 @@ Path::Path(const fs::path & from)
 {}
 
 //===========================================================================
+Path & Path::clear() {
+    m_data.clear();
+    return *this;
+}
+
+//===========================================================================
+void Path::swap(Path & from) {
+    ::swap(m_data, from.m_data);
+}
+
+//===========================================================================
+Path & Path::assign(std::string_view path) {
+    m_data = path;
+    //normalize();
+    return *this;
+}
+
+//===========================================================================
+Path & Path::assign(std::string_view path, std::string_view defExt) {
+    return assign(path).defaultExt(defExt);
+}
+
+//===========================================================================
+Path & Path::assign(const fs::path & path) {
+    return assign(string_view{path.generic_u8string()});
+}
+
+//===========================================================================
+Path & Path::assign(const fs::path & path, std::string_view defExt) {
+    return assign(path).defaultExt(defExt);
+}
+
+//===========================================================================
+Path & Path::setRootName(char drive) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::setRootName(std::string_view root) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::setDir(std::string_view dir) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::setParentPath(std::string_view path) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::setFilename(std::string_view filename) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::setFilename(std::string_view filename, std::string_view defExt) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::setStem(std::string_view stem) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::setStem(std::string_view stem, std::string_view ext) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::setExt(std::string_view ext) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::defaultExt(std::string_view defExt) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::concat(std::string_view path) {
+    Count cnt(m_data);
+    return *this;
+}
+
+//===========================================================================
+Path & Path::resolve(const Path & base) {
+    return resolve(base.m_data);
+}
+
+//===========================================================================
+Path & Path::resolve(std::string_view base) {
+    return *this;
+}
+
+//===========================================================================
 fs::path Path::fsPath() const {
     return fs::u8path(m_data);
 }
@@ -42,4 +163,97 @@ fs::path Path::fsPath() const {
 //===========================================================================
 std::string_view Path::view() const {
     return m_data;
+}
+
+//===========================================================================
+const char * Path::c_str() const {
+    return m_data.c_str();
+}
+
+//===========================================================================
+size_t Path::size() const {
+    return m_data.size();
+}
+
+//===========================================================================
+std::string_view Path::rootName() const {
+    Count cnt(m_data);
+    return {m_data.data(), cnt.m_rootLen};
+}
+
+//===========================================================================
+std::string_view Path::dir() const {
+    Count cnt(m_data);
+    return {m_data.data() + cnt.m_rootLen, cnt.m_dirLen};
+}
+
+//===========================================================================
+std::string_view Path::parentPath() const {
+    Count cnt(m_data);
+    return {m_data.data(), cnt.m_rootLen + cnt.m_dirLen};
+}
+
+//===========================================================================
+std::string_view Path::filename() const {
+    Count cnt(m_data);
+    return {
+        m_data.data() + cnt.m_rootLen + cnt.m_dirLen, 
+        cnt.m_stemLen + cnt.m_extLen
+    };
+}
+
+//===========================================================================
+std::string_view Path::stem() const {
+    Count cnt(m_data);
+    return {m_data.data() + cnt.m_rootLen + cnt.m_dirLen, cnt.m_stemLen};
+}
+
+//===========================================================================
+std::string_view Path::extension() const {
+    Count cnt(m_data);
+    return {
+        m_data.data() + cnt.m_rootLen + cnt.m_dirLen + cnt.m_stemLen,
+        cnt.m_extLen
+    };
+}
+
+//===========================================================================
+bool Path::empty() const {
+    return m_data.empty();
+}
+
+//===========================================================================
+bool Path::hasRootName() const {
+    Count cnt(m_data);
+    return cnt.m_rootLen;
+}
+
+//===========================================================================
+bool Path::hasRootDir() const {
+    Count cnt(m_data);
+    return cnt.m_dirLen && m_data[cnt.m_rootLen] == '/';
+}
+
+//===========================================================================
+bool Path::hasDir() const {
+    Count cnt(m_data);
+    return cnt.m_dirLen;
+}
+
+//===========================================================================
+bool Path::hasFilename() const {
+    Count cnt(m_data);
+    return cnt.m_stemLen + cnt.m_extLen;
+}
+
+//===========================================================================
+bool Path::hasStem() const {
+    Count cnt(m_data);
+    return cnt.m_stemLen;
+}
+
+//===========================================================================
+bool Path::hasExt() const {
+    Count cnt(m_data);
+    return cnt.m_extLen;
 }
--- libs/file/path.h
+++ libs/file/path.h
@@ -24,12 +24,66 @@ public:
     Path(std::string_view from);
     Path(const std::experimental::filesystem::path & from);
 
+    Path & clear();
+    void swap(Path & from);
+
+    Path & assign(std::string_view path);
+    Path & assign(std::string_view path, std::string_view defExt);
+    Path & assign(const std::experimental::filesystem::path & path);
+    Path & assign(
+        const std::experimental::filesystem::path & path, 
+        std::string_view defExt
+    );
+    Path & operator=(const Path & from) { return assign(from.view()); }
+    Path & operator=(std::string_view from) { return assign(from); }
+    Path & operator=(const std::experimental::filesystem::path & from) { 
+        return assign(from); 
+    }
+
+    Path & setRootName(char drive);
+    Path & setRootName(std::string_view root);
+    Path & setDir(std::string_view dir);
+    Path & setParentPath(std::string_view path);
+    Path & setFilename(std::string_view filename);
+    Path & setFilename(std::string_view filename, std::string_view defExt);
+    Path & setStem(std::string_view stem);
+    Path & setStem(std::string_view stem, std::string_view ext);
+    Path & setExt(std::string_view ext);
+    Path & defaultExt(std::string_view defExt);
+
+    Path & removeFilename() { return setFilename({}); }
+
+    Path & concat(std::string_view path);
+    Path & operator+=(std::string_view path) { return concat(path); }
+
+    // resolve path relative to base
+    Path & resolve(const Path & base);
+    Path & resolve(std::string_view base);
+    Path & operator/=(std::string_view path) { return resolve(path); }
+
+    explicit operator bool() const { return !empty(); }
+
     std::experimental::filesystem::path fsPath() const;
     std::string_view view() const;
-private:
-    // intentionally not implemented
-    operator std::experimental::filesystem::path();
+    const char * c_str() const;
+    size_t size() const;
 
+    std::string_view rootName() const;
+    std::string_view dir() const;
+    std::string_view parentPath() const;
+    std::string_view filename() const;
+    std::string_view stem() const;
+    std::string_view extension() const;
+
+    bool empty() const;
+    bool hasRootName() const;
+    bool hasRootDir() const;
+    bool hasDir() const;
+    bool hasFilename() const;
+    bool hasStem() const;
+    bool hasExt() const;
+
+private:
     std::string m_data;
 };
 
