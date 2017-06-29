26237632bd22128eab16e4f40a8f1c3defc1b32d
gknowles <gknowles@ieee.org>
2017-06-28 22:04:38 -0700

    Minor cleanup

--- libs/app/pch.h
+++ libs/app/pch.h
@@ -16,18 +16,13 @@
 #include "xml/xml.h"
 
 // Standard headers
-#include <array>
-#include <atomic>
 #include <cassert>
 #include <condition_variable>
 #include <functional>
 #include <iomanip>
 #include <iostream>
-#include <map>
 #include <memory>
 #include <mutex>
-#include <queue>
-#include <random>
 #include <thread>
 #include <vector>
 
