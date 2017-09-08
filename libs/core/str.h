0a236aa83ba50179d88abb43b8f59b39a73187c2
gknowles <gknowles@ieee.org>
2017-09-08 06:32:07 -0700

    Change stringTo to use "if constexpr" instead of template overloads

--- libs/core/str.h
+++ libs/core/str.h
@@ -161,31 +161,18 @@ uint64_t strToUint64(
 //===========================================================================
 // stringTo - converts from string to T
 //===========================================================================
-template <typename T>
-auto stringTo_impl(T & out, const std::string & src, int)
-    -> decltype(out = src, bool()) {
-    out = src;
-    return true;
-}
-
-//===========================================================================
-template <typename T>
-bool stringTo_impl(T & out, const std::string & src, long) {
-    std::stringstream interpreter;
-    if (!(interpreter << src) || !(interpreter >> out)
-        || !(interpreter >> std::ws).eof()) {
-        out = {};
-        return false;
+template <typename T> bool stringTo(T & out, const std::string & src) {
+    if constexpr(std::is_assignable_v<T, const std::string&>) {
+        out = src;
+    } else {
+        std::stringstream interpreter;
+        if (!(interpreter << src) || !(interpreter >> out)
+            || !(interpreter >> std::ws).eof()) {
+            out = {};
+            return false;
+        }
     }
     return true;
-}
-
-//===========================================================================
-template <typename T> bool stringTo(T & out, const std::string & src) {
-    // prefer the version of stringTo_impl taking an int as it's third
-    // parameter, if that doesn't exist for T (because no out=src assignment
-    // operator exists) the version taking a long is called.
-    return stringTo_impl(out, src, 0);
 }
 
 
