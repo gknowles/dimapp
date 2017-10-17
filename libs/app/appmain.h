752456a9fd1cddfd47e933775c29e407a872a7d8
gknowles <gknowles@ieee.org>
2017-10-16 22:47:21 -0700

    Fix, compile error (missing EX__APPBASE)
    Cleanup program exit codes.

--- libs/app/appmain.h
+++ libs/app/appmain.h
@@ -120,26 +120,37 @@ bool appDataPath(std::string & out, std::string_view file, bool cine = true);
 ***/
 
 // program exit codes
-// On *nix the EX_* constants are defined in <sysexits.h>
 enum {
     EX_OK = 0,
 
-    // Exit code is being set by an asyncronously event that hasn't completed
-    EX_PENDING = 32,
+    // Microsoft specific
+    EX_ABORTED = 3, // used by CRT in assert() failures and abort()
 
-    // sysexits.h
+    // Start of exit codes defined by this application
+    EX__APPBASE = 10,
+
+    // Exit code is being set by an asyncronously event that hasn't completed,
+    // prevents appSignalUsageError() from taking action.
+    EX_PENDING = 63,
+
+    // On *nix the following EX_* constants are defined in <sysexits.h>
+    // These are centered around why a command line utility might fail.
     EX__BASE = 64,       // base value for standard error codes
     EX_USAGE = 64,       // bad command line
     EX_DATAERR = 65,     // bad input file
+    EX_NOINPUT = 66,     // missing or inaccessible input file
+    EX_NOUSER = 67,      // user doesn't exist
+    EX_NOHOST = 68,      // host doesn't exist (not merely connect failed)
     EX_UNAVAILABLE = 69, // failed for some other reason
     EX_SOFTWARE = 70,    // internal software error
     EX_OSERR = 71,       // some os operation failed
+    EX_OSFILE = 72,      // some operation on a system file failed
+    EX_CANTCREAT = 73,   // can't create user supplied output file
     EX_IOERR = 74,       // console or file read/write error
+    EX_TEMPFAIL = 75,    // temporary failure, trying again later may work
+    EX_PROTOCOL = 76,    // illegal response from remote system
     EX_NOPERM = 77,      // insufficient permission
     EX_CONFIG = 78,      // configuration error
-
-    // Microsoft specific
-    EX_ABORTED = 3, // used by CRT in assert() failures and abort()
 };
 
 // Signals shutdown to begin, the exitcode will be return from appRun() after
@@ -157,6 +168,9 @@ void appSignalShutdown(int exitcode = EX_OK);
 //
 // Unless the application is running as a service, the error is logged to the 
 // console in addition to any other log targets.
+//
+// If exitcode is EX_PENDING appSignalUsageError() returns immediately without
+// doing anything.
 void appSignalUsageError(
     std::string_view err = {},
     std::string_view detail = {}
