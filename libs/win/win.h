f5edcebae2b33e24a7193f2361846ed5dde8923b
gknowles <gknowles@ieee.org>
2018-07-02 14:52:55 -0700

    Change winSvcInstall from lotsa params to giant struct param.

--- libs/win/win.h
+++ libs/win/win.h
@@ -6,6 +6,8 @@
 
 #include "cppconf/cppconf.h"
 
+#include "core/time.h"
+
 #include <vector>
 
 namespace Dim {
@@ -17,14 +19,104 @@ namespace Dim {
 *
 ***/
 
-bool winSvcInstall(
-    const char path[],
-    const char name[],
-    const char displayName[],
-    const char desc[],
-    const std::vector<const char *> & deps,
-    const char account[],
-    const char password[]
-);
+struct WinServiceConfig {
+    enum class Type {
+        kInvalid,
+        kOwn,
+        kShare,
+        kKernel,
+        kFileSys,
+        kRecognizer,
+        kAdapter,
+        kUserOwn,
+        kUserShare,
+
+        kDefault = kOwn,
+    };
+    enum class Start {
+        kInvalid,
+        kAuto,
+        kAutoDelayed,
+        kBoot,
+        kDemand,
+        kDisabled,
+        kSystem,
+
+        kDefault = kAuto,
+    };
+    enum class ErrCtrl {
+        kInvalid,
+        kIgnore,
+        kNormal,
+        kSevere,
+        kCritical,
+
+        kDefault = kNormal,
+    };
+    struct Action {
+        enum Type {
+            kInvalid,
+            kNone,
+            kRestart,
+            kReboot,
+            kRunCommand,
+        };
+        Type type {};
+        Duration delay;
+    };
+    enum class SidType {
+        kInvalid,
+        kNone,
+        kUnrestricted,
+        kRestricted,
+
+        kDefault = kNone,
+    };
+    struct Trigger {
+        enum Type {
+            kInvalid,
+            kCustom,
+            kDeviceInterfaceArrival,
+            kDomainJoin,
+            kFirewallPortEvent,
+            kGroupPolicy,
+            kIpAddressAvail,
+            kNetworkEndpoint,
+        };
+        enum Action {
+            kServiceStart,
+            kServiceStop,
+        };
+    };
+
+    const char * serviceName {nullptr};
+    Type serviceType {};
+    Start startType {};
+    ErrCtrl errorControl {};
+    const char * progWithArgs {nullptr};
+    const char * loadOrderGroup {nullptr};
+    unsigned loadOrderTag {};
+    std::vector<const char *> deps;
+    const char * account {nullptr};
+    const char * password {nullptr};
+    const char * displayName {nullptr};
+    const char * desc {nullptr};
+
+    bool failureFlag {false};
+    Duration failureReset;
+    const char * rebootMsg {nullptr}; // used by FailAction::kReboot
+    const char * failureProgWithArgs {nullptr};
+    std::vector<Action> failureActions;
+
+    unsigned preferredNode = (unsigned) -1; // -1 for none
+    Duration preshutdownTimeout;
+    std::vector<const char *> privs;
+    SidType sidType {};
+
+    // NOTE: TRIGGERS NOT SUPPORTED
+    std::vector<Trigger> triggers;
+};
+
+bool winSvcInstall(const WinServiceConfig & sconf);
 
 } // namespace
--- libs/win/winsvcctrl.cpp
+++ libs/win/winsvcctrl.cpp
@@ -6,21 +6,8 @@
 #pragma hdrstop
 
 using namespace std;
+using namespace std::chrono;
 using namespace Dim;
-
-
-/****************************************************************************
-*
-*   Variables
-*
-***/
-
-
-/****************************************************************************
-*
-*   Helpers
-*
-***/
 
 
 /****************************************************************************
@@ -30,20 +17,14 @@ using namespace Dim;
 ***/
 
 //===========================================================================
-bool Dim::winSvcInstall(
-    const char path[],
-    const char name[],
-    const char displayName[],
-    const char desc[],
-    const vector<const char *> & deps,
-    const char account[],
-    const char password[]
-) {
-    if (!path)
-        path = envExecPath().c_str();
-    if (!displayName)
-        displayName = name;
-    assert(name && *name);
+bool Dim::winSvcInstall(const WinServiceConfig & sconf) {
+    auto conf{sconf};
+
+    if (!conf.progWithArgs)
+        conf.progWithArgs = envExecPath().c_str();
+    if (!conf.displayName)
+        conf.displayName = conf.serviceName;
+    assert(conf.serviceName && *conf.serviceName);
     auto scm = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
     if (!scm) {
         logMsgError() << "OpenSCManager(CREATE_SERVICE): " << WinError{};
@@ -52,25 +33,25 @@ bool Dim::winSvcInstall(
     Finally scm_f([=]() { CloseServiceHandle(scm); });
 
     CharBuf depv;
-    for (auto && dep : deps) {
+    for (auto && dep : conf.deps) {
         depv.append(dep).append(1, '\0');
     }
     depv.append(1, '\0');
 
     auto s = CreateService(
         scm,
-        name,
-        displayName,
+        conf.serviceName,
+        conf.displayName,
         SERVICE_ALL_ACCESS,
         SERVICE_WIN32_OWN_PROCESS,
         SERVICE_AUTO_START,
         SERVICE_ERROR_NORMAL,
-        path,
+        conf.progWithArgs,
         NULL,   // load order group
         NULL,   // load order group tag
         depv.data(),
-        account,
-        password
+        conf.account,
+        conf.password
     );
     if (!s) {
         logMsgError() << "CreateService: " << WinError{};
@@ -79,9 +60,9 @@ bool Dim::winSvcInstall(
     Finally s_f([=]() { CloseServiceHandle(s); });
     Finally s_d([=]() { DeleteService(s); });
 
-    if (desc) {
+    if (conf.desc) {
         SERVICE_DESCRIPTION sd;
-        sd.lpDescription = (char *) desc;
+        sd.lpDescription = (char *) conf.desc;
         if (!ChangeServiceConfig2(s, SERVICE_CONFIG_DESCRIPTION, &sd)) {
             logMsgError() << "ChangeServiceConfig2(DESCRIPTION): "
                 << WinError{};
@@ -89,23 +70,32 @@ bool Dim::winSvcInstall(
         }
     }
 
-    SERVICE_FAILURE_ACTIONS_FLAG faf;
-    faf.fFailureActionsOnNonCrashFailures = true;
-    if (!ChangeServiceConfig2(s, SERVICE_CONFIG_FAILURE_ACTIONS_FLAG, &faf)) {
-        logMsgError() << "ChangeServiceConfig2(FAILURE_FLAG): " << WinError{};
-        return false;
+    if (conf.failureFlag) {
+        SERVICE_FAILURE_ACTIONS_FLAG faf;
+        faf.fFailureActionsOnNonCrashFailures = true;
+        if (!ChangeServiceConfig2(s, SERVICE_CONFIG_FAILURE_ACTIONS_FLAG, &faf)) {
+            logMsgError() << "ChangeServiceConfig2(FAILURE_FLAG): " << WinError{};
+            return false;
+        }
     }
 
-    SC_ACTION facts[] = {
-        { SC_ACTION_RESTART, 10 * 1000 }, // 10 seconds, in milliseconds
-        { SC_ACTION_RESTART, 60 * 1000 }, // 1 minute, in milliseconds
-        { SC_ACTION_RESTART, 10 * 60 * 1000 }, // 10 minutes, in milliseconds
-    };
+    vector<SC_ACTION> facts;
+    for (auto && fa : conf.failureActions) {
+        auto & sc = facts.emplace_back();
+        sc.Delay = (DWORD) duration_cast<milliseconds>(fa.delay).count();
+        switch (fa.type) {
+        case fa.kInvalid: [[fallthrough]];
+        case fa.kNone: sc.Type = SC_ACTION_NONE; break;
+        case fa.kRestart: sc.Type = SC_ACTION_RESTART; break;
+        case fa.kReboot: sc.Type = SC_ACTION_REBOOT; break;
+        case fa.kRunCommand: sc.Type = SC_ACTION_RUN_COMMAND; break;
+        }
+    }
     SERVICE_FAILURE_ACTIONS fa = {};
-    fa.dwResetPeriod = 24 * 60 * 60; // 1 day, in seconds
-    fa.lpCommand = (char *) "";
+    fa.dwResetPeriod =
+        (DWORD) duration_cast<seconds>(conf.failureReset).count();
     fa.cActions = (DWORD) size(facts);
-    fa.lpsaActions = facts;
+    fa.lpsaActions = data(facts);
     if (!ChangeServiceConfig2(s, SERVICE_CONFIG_FAILURE_ACTIONS, &fa)) {
         logMsgError() << "ChangeServiceConfig2(FAILURE_ACTIONS): "
             << WinError{};
@@ -116,4 +106,3 @@ bool Dim::winSvcInstall(
     logMsgInfo() << "Service created.";
     return true;
 }
-
