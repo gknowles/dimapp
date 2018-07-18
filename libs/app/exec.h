bcb10769f6fe89bc51531f4ebb73a0fab477f05d
gknowles <gknowles@ieee.org>
2018-07-18 13:25:49 -0700

    Add support for executing child process

--- libs/app/exec.h
+++ libs/app/exec.h
@@ -6,12 +6,73 @@
 
 #include "cppconf/cppconf.h"
 
-#include <string>
+#include "core/core.h"
+#include "file/file.h"
+
+#include <string_view>
 
 namespace Dim {
 
+enum StdStream {
+    kStdIn,
+    kStdOut,
+    kStdErr,
+};
+
+class IExecNotify {
+public:
+    virtual ~IExecNotify() = default;
+
+    virtual void onExecComplete(bool canceled, int exitCode) = 0;
+
+    // Default implementation accumulates the output into m_out & m_err
+    // Returning false stops reading stream
+    virtual bool onExecRead(
+        size_t * bytesUsed,
+        StdStream strm, // kStdOut or kStdErr
+        std::string_view data
+    );
+
+protected:
+    CharBuf m_out;
+    CharBuf m_err;
+
+private:
+    friend class ExecProgram;
+    ExecProgram * m_exec;
+};
+
+
+void execProgram(
+    IExecNotify * notify,
+    std::string_view prog,
+    std::string_view args
+);
+
+// Write handle to stdin of child process
+void execWrite(IExecNotify * notify, std::string_view data);
+
+void execCancel(IExecNotify * notify);
+
+
+void execListen(
+    IExecNotify * notify,
+    std::string * pipeName,
+    std::string * secret
+);
+void execClientAttach(std::string_view pipeName, std::string_view secret);
+
+
+bool execProgramWait(
+    int * exitCode,
+    CharBuf * out,
+    CharBuf * err,
+    std::string_view prog,
+    std::string_view args
+);
+
 // Waits for program to complete and returns its exit code, returns -1
 // if unable to execute program.
-int execElevated(std::string_view prog, std::string_view args);
+bool execElevated(int * exitCode, std::string_view prog, std::string_view args);
 
 } // namespace
--- libs/win/winexec.cpp
+++ libs/win/winexec.cpp
@@ -11,12 +11,344 @@ using namespace Dim;
 
 /****************************************************************************
 *
-*   Public API
+*   Declarations
+*
+***/
+
+namespace Dim {
+
+class ExecProgram : public IWinEventWaitNotify {
+public:
+    class ExecPipe : public IPipeNotify {
+    public:
+        // Inherited via IPipeNotify
+        bool onPipeRead(size_t * bytesUsed, string_view data) override;
+        void onPipeDisconnect() override;
+
+        ExecProgram * m_notify{nullptr};
+        StdStream m_strm;
+        bool m_closed{false};
+    };
+
+public:
+    ExecProgram(IExecNotify * notify);
+    ~ExecProgram();
+    void exec(string_view prog, string_view args);
+
+    bool onRead(size_t * bytesUsed, StdStream strm, string_view data);
+    void onDisconnect(StdStream strm);
+
+    // Inherited via IWinEventWaitNotify
+    // Triggered when child process exits
+    void onTask() override;
+
+private:
+    bool createPipe(
+        HANDLE * hchild,
+        StdStream strm,
+        const Path & name,
+        Pipe::OpenMode oflags
+    );
+    void checkIfDone();
+
+    IExecNotify * m_notify{nullptr};
+    ExecPipe m_pipes[3];
+    unsigned m_connected{0};
+    HANDLE m_process;
+
+    char m_outbuf[4096];
+    char m_errbuf[4096];
+
+    RunMode m_mode{kRunStarting};
+    bool m_canceled{false};
+    int m_exitCode{0};
+};
+
+} // namespace
+
+
+/****************************************************************************
+*
+*   ExecProgram::ExecPipe
 *
 ***/
 
 //===========================================================================
-int Dim::execElevated(string_view prog, string_view args) {
+bool ExecProgram::ExecPipe::onPipeRead(
+    size_t * bytesUsed,
+    std::string_view data
+) {
+    return m_notify->onRead(bytesUsed, m_strm, data);
+}
+
+//===========================================================================
+void ExecProgram::ExecPipe::onPipeDisconnect() {
+    m_notify->onDisconnect(m_strm);
+}
+
+
+/****************************************************************************
+*
+*   ExecProgram
+*
+***/
+
+//===========================================================================
+ExecProgram::ExecProgram(IExecNotify * notify)
+    : m_notify(notify)
+{}
+
+//===========================================================================
+ExecProgram::~ExecProgram() {
+    for (auto && pi : m_pipes)
+        pipeClose(&pi);
+    CloseHandle(m_process);
+    if (m_notify)
+        m_notify->m_exec = nullptr;
+}
+
+//===========================================================================
+void ExecProgram::exec(string_view prog, string_view args) {
+    char name[100];
+    snprintf(
+        name,
+        sizeof(name),
+        "//./pipe/local/%u_%p",
+        envProcessId(),
+        this
+    );
+
+    struct {
+        StdStream strm;
+        Path name;
+        Pipe::OpenMode oflags;
+        HANDLE child;
+    } pipes[] = {
+        { kStdIn, Path(name).setExt("in"), Pipe::fWriteOnly },
+        { kStdOut, Path(name).setExt("out"), Pipe::fReadOnly },
+        { kStdErr, Path(name).setExt("err"), Pipe::fReadOnly },
+    };
+    for (auto && p : pipes) {
+        if (!createPipe(&p.child, p.strm, p.name, p.oflags)) {
+            for (auto && p2 : pipes) {
+                if (p2.child)
+                    CloseHandle(p2.child);
+            }
+            return m_notify->onExecComplete(true, -1);
+        }
+    }
+
+    auto wprog = toWstring(prog);
+    auto wargs = toWstring(args);
+    STARTUPINFOW si = { sizeof(si) };
+    si.dwFlags = STARTF_USESTDHANDLES;
+    si.hStdInput = pipes[kStdIn].child;
+    si.hStdOutput = pipes[kStdOut].child;
+    si.hStdError = pipes[kStdErr].child;
+    PROCESS_INFORMATION pi;
+    bool running = CreateProcessW(
+        wprog.c_str(),
+        wargs.data(),
+        NULL,   // process attrs
+        NULL,   // thread attrs
+        true,
+        CREATE_NEW_PROCESS_GROUP,
+        NULL, // environment
+        NULL, // current dir
+        &si,
+        &pi
+    );
+    for (auto && p : pipes)
+        CloseHandle(p.child);
+    if (!running)
+        return m_notify->onExecComplete(true, -1);
+
+    m_process = pi.hProcess;
+    registerWait(pi.hProcess);
+}
+
+//===========================================================================
+bool ExecProgram::onRead(
+    size_t * bytesUsed,
+    StdStream strm,
+    string_view data
+) {
+    return m_notify->onExecRead(bytesUsed, strm, data);
+}
+
+//===========================================================================
+void ExecProgram::onDisconnect(StdStream strm) {
+    auto & pi = m_pipes[strm];
+    pi.m_closed = true;
+    checkIfDone();
+}
+
+//===========================================================================
+void ExecProgram::onTask() {
+    m_mode = kRunStopped;
+    DWORD rc;
+    if (GetExitCodeProcess(m_process, &rc)) {
+        m_canceled = false;
+        m_exitCode = rc;
+    } else {
+        m_canceled = true;
+        m_exitCode = -1;
+    }
+    checkIfDone();
+}
+
+//===========================================================================
+void ExecProgram::checkIfDone() {
+    if (m_pipes[kStdOut].m_closed
+        && m_pipes[kStdErr].m_closed
+        && m_mode == kRunStopped
+    ) {
+        m_notify->onExecComplete(m_canceled, m_exitCode);
+        delete this;
+    }
+}
+
+//===========================================================================
+bool ExecProgram::createPipe(
+    HANDLE * child,
+    StdStream strm,
+    const Path & name,
+    Pipe::OpenMode oflags
+) {
+    pipeListen(&m_pipes[strm], name, oflags);
+
+    if (child) {
+        auto wname = toWstring(name);
+        SECURITY_ATTRIBUTES sa = {};
+        sa.bInheritHandle = true;
+        auto cp = CreateFileW(
+            wname.c_str(),
+            oflags == Pipe::fReadOnly ? GENERIC_READ : GENERIC_WRITE,
+            0,      // share
+            &sa,
+            OPEN_EXISTING,
+            FILE_FLAG_OVERLAPPED,
+            NULL    // template file
+        );
+        if (cp == INVALID_HANDLE_VALUE) {
+            logMsgError() << "CreateFile(pipe): " << WinError{};
+            return false;
+        }
+        *child = cp;
+    }
+
+    return true;
+}
+
+
+/****************************************************************************
+*
+*   IExecNotify
+*
+***/
+
+//===========================================================================
+bool IExecNotify::onExecRead(
+    size_t * bytesUsed,
+    StdStream strm,
+    string_view data
+) {
+    switch (strm) {
+    case kStdIn: break;
+    case kStdOut: m_out.append(data); break;
+    case kStdErr: m_err.append(data); break;
+    }
+    *bytesUsed = data.size();
+    return true;
+}
+
+
+/****************************************************************************
+*
+*   Execute child program
+*
+***/
+
+//===========================================================================
+void Dim::execProgram(
+    IExecNotify * notify,
+    std::string_view prog,
+    std::string_view args
+) {
+    assert(notify);
+
+    char bytes[8];
+    cryptRandomBytes(bytes, sizeof(bytes));
+
+    auto ep = make_unique<ExecProgram>(notify);
+
+}
+
+
+/****************************************************************************
+*
+*   Execute child program and wait
+*
+***/
+
+namespace {
+
+class ExecWaitNotify : public IExecNotify {
+public:
+    void onExecComplete(bool canceled, int exitCode) override;
+
+    bool wait(int * exitCode, CharBuf * out, CharBuf * err);
+
+private:
+    mutex m_mut;
+    condition_variable m_cv;
+
+    bool m_complete{false};
+    bool m_canceled{false};
+    int m_exitCode{0};
+};
+
+} // namespace
+
+//===========================================================================
+void ExecWaitNotify::onExecComplete(bool canceled, int exitCode) {
+    m_canceled = canceled;
+    m_exitCode = exitCode;
+}
+
+//===========================================================================
+bool ExecWaitNotify::wait(int * exitCode, CharBuf * out, CharBuf * err) {
+    unique_lock lk{m_mut};
+    while (!m_complete)
+        m_cv.wait(lk);
+    *out = move(m_out);
+    *err = move(m_err);
+    *exitCode = m_exitCode;
+    return !m_canceled;
+}
+
+//===========================================================================
+bool Dim::execProgramWait(
+    int * exitCode,
+    CharBuf * out,
+    CharBuf * err,
+    std::string_view prog,
+    std::string_view args
+) {
+    ExecWaitNotify notify;
+    execProgram(&notify, prog, args);
+    return notify.wait(exitCode, out, err);
+}
+
+
+/****************************************************************************
+*
+*   Elevated
+*
+***/
+
+//===========================================================================
+bool Dim::execElevated(int * exitCode, string_view prog, string_view args) {
     SHELLEXECUTEINFOW ei = { sizeof(ei) };
     ei.lpVerb = L"RunAs";
     auto wexe = toWstring(prog);
@@ -35,11 +367,13 @@ int Dim::execElevated(string_view prog, string_view args) {
         } else {
             logMsgError() << "ShellExecuteExW: " << WinError();
         }
-        return -1;
+        *exitCode = -1;
+        return false;
     }
 
     DWORD rc = EX_OSERR;
     if (WAIT_OBJECT_0 == WaitForSingleObject(ei.hProcess, INFINITE))
         GetExitCodeProcess(ei.hProcess, &rc);
-    return rc;
+    *exitCode = rc;
+    return true;
 }
