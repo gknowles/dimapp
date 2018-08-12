327c99995f43c5f02e2d359ff72565539b08da9d
gknowles <gknowles@ieee.org>
2018-08-12 00:44:18 -0700

    Tweak "Abnormal abort" message.

--- libs/win/wincrash.cpp
+++ libs/win/wincrash.cpp
@@ -123,7 +123,7 @@ static void writeDump() {
 
 //===========================================================================
 extern "C" void abortHandler(int sig) {
-    printf("abnormal abort, sig %d\n", sig);
+    printf("Abnormal abort, sig %d\n", sig);
     scoped_lock lk{s_mut};
 
     if (IsDebuggerPresent())
