e40cce2ea6a270162863b27a29bcccd6129f2b96
gknowles <gknowles@ieee.org>
2018-10-30 16:23:39 -0700

    Add /srv/logfiles.json and /srv/logfiles/...

--- libs/app/logfile.cpp
+++ libs/app/logfile.cpp
@@ -210,6 +210,168 @@ void ConfigAppXml::onConfigChange(const XDocument & doc) {
 
 /****************************************************************************
 *
+*   JsonLogFiles
+*
+***/
+
+namespace {
+class JsonLogFiles : public IHttpRouteNotify {
+    void onHttpRequest(unsigned reqId, HttpRequest & msg) override;
+};
+} // namespace
+
+//===========================================================================
+void JsonLogFiles::onHttpRequest(unsigned reqId, HttpRequest & msg) {
+    auto now = timeNow();
+    HttpResponse res;
+    JBuilder bld(&res.body());
+    bld.object();
+    bld.member("now", now);
+    bld.member("files").array();
+    auto logDir = appLogDir().view();
+    for (auto && f : FileIter(logDir)) {
+        auto rname = f.path.view();
+        rname.remove_prefix(logDir.size() + 1);
+        bld.object();
+        bld.member("name", rname);
+        bld.member("mtime", f.mtime);
+        bld.member("size", fileSize(f.path));
+        bld.end();
+    }
+    bld.end();
+    bld.end();
+    res.addHeader(kHttpContentType, "application/json");
+    res.addHeader(kHttp_Status, "200");
+    httpRouteReply(reqId, move(res));
+}
+
+
+/****************************************************************************
+*
+*   JsonLogTail
+*
+***/
+
+namespace {
+class JsonLogTail : public IHttpRouteNotify {
+public:
+    struct LogJob : IFileReadNotify {
+        unsigned m_reqId {0};
+        char m_buffer[8'192];
+        unsigned m_limit {0};
+        unsigned m_found {0};
+        size_t m_endPos {0};
+
+        bool onFileRead(
+            size_t * bytesUsed,
+            string_view data,
+            int64_t offset,
+            FileHandle f
+        ) override;
+        void onFileEnd(int64_t offset, FileHandle f) override;
+    };
+public:
+    void onHttpRequest(unsigned reqId, HttpRequest & msg) override;
+private:
+    Param & m_limit = param("limit");
+};
+} // namespace
+
+//===========================================================================
+void JsonLogTail::onHttpRequest(unsigned reqId, HttpRequest & msg) {
+    auto now = timeNow();
+    auto qpath = msg.query().path;
+    auto prefix = httpRouteGetInfo(reqId).path.size();
+    qpath.remove_prefix(prefix);
+    Path path;
+    if (!appLogPath(&path, qpath, false))
+        return httpRouteReplyNotFound(reqId, msg);
+
+    mapParams(msg);
+    auto limit = clamp(strToUint(*m_limit), 10u, 10'000u);
+
+    auto file = fileOpen(path, File::fReadOnly | File::fDenyNone);
+    if (!file) {
+        HttpResponse res;
+        res.addHeader(kHttpContentType, "text/plain");
+        res.addHeader(kHttp_Status, "200");
+        httpRouteReply(reqId, move(res));
+        return;
+    }
+    auto job = new LogJob;
+    job->m_reqId = reqId;
+    job->m_limit = limit;
+    job->m_endPos = fileSize(file);
+    auto pos = job->m_endPos > size(job->m_buffer)
+        ? job->m_endPos - size(job->m_buffer)
+        : 0;
+    fileRead(job, job->m_buffer, size(job->m_buffer), file, pos);
+}
+
+//===========================================================================
+bool JsonLogTail::LogJob::onFileRead(
+    size_t * bytesUsed,
+    string_view data,
+    int64_t offset,
+    FileHandle f
+) {
+    vector<string_view> lines;
+    split(&lines, data, "\r\n");
+    if (m_found < m_limit) {
+        m_found += (unsigned) lines.size() - 1;
+        if (m_found < m_limit && offset > 0) {
+            auto pos = offset > (int64_t) size(m_buffer)
+                ? offset - size(m_buffer)
+                : 0;
+            fileRead(this, m_buffer, size(m_buffer), f, pos);
+            return false;
+        }
+        HttpResponse res;
+        res.addHeader(kHttpContentType, "text/plain");
+        res.addHeader(kHttp_Status, "200");
+        httpRouteReply(m_reqId, move(res), true);
+        auto i = lines.begin() + m_found - m_limit;
+        auto e = lines.end() - 1;
+        assert(i <= e);
+        for (; i < e; ++i) {
+            *const_cast<char *>(&*i->end()) = '\n';
+            httpRouteReply(m_reqId, {i->data(), i->size() + 1}, true);
+        }
+        httpRouteReply(m_reqId, lines.back(), m_found > lines.size());
+        m_limit = m_found - (unsigned) lines.size() + 1;
+        m_found = m_limit;
+        if (m_found) {
+            auto pos = offset + data.size();
+            auto len = m_endPos - pos;
+            fileRead(this, m_buffer, size(m_buffer), f, pos, len);
+        }
+        return false;
+    }
+
+    auto i = lines.begin();
+    auto e = lines.end() - 1;
+    for (; i < e; ++i) {
+        *const_cast<char *>(&*i->end()) = '\n';
+        httpRouteReply(m_reqId, {i->data(), i->size() + 1}, true);
+        m_limit -= 1;
+    }
+    m_found = m_limit;
+    httpRouteReply(m_reqId, lines.back(), m_found);
+    *bytesUsed = data.size();
+    return true;
+}
+
+//===========================================================================
+void JsonLogTail::LogJob::onFileEnd(int64_t offset, FileHandle f) {
+    if (!m_limit) {
+        fileClose(f);
+        delete this;
+    }
+}
+
+
+/****************************************************************************
+*
 *   ShutdownNotify
 *
 ***/
@@ -244,6 +406,9 @@ void ShutdownNotify::onShutdownConsole(bool firstTry) {
 *
 ***/
 
+static JsonLogFiles s_jsonLogFiles;
+static JsonLogTail s_jsonLogTail;
+
 //===========================================================================
 void Dim::iLogFileInitialize() {
     shutdownMonitor(&s_cleanup);
@@ -253,6 +418,8 @@ void Dim::iLogFileInitialize() {
         logMsgFatal() << "Invalid log path: " << s_logfile;
 
     logMonitor(&s_logger);
+    httpRouteAdd(&s_jsonLogFiles, "/srv/logfiles.json");
+    httpRouteAdd(&s_jsonLogTail, "/srv/logfiles/", fHttpMethodGet, true);
 }
 
 //===========================================================================
