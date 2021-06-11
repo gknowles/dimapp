f167beb946894d025295b6dccde7aff7b5b66743
gknowles <gknowles@ieee.org>
2021-06-11 03:26:24 -0700

    Change initial cmake run to be non-recursive.
    Saves a little time only search for configured compilers once.

--- configure.bat
+++ configure.bat
@@ -1,7 +1,9 @@
-:: Copyright Glen Knowles 2016 - 2019.
+:: Copyright Glen Knowles 2016 - 2021.
 :: Distributed under the Boost Software License, Version 1.0.
 @echo off
 if exist build rd /s/q build
 md build & cd build
-cmake -G "Visual Studio 16 2019" -A x64 -T v142 ..
+cmake -G "Visual Studio 16 2019" -A x64 -T v142 .. ^
+    -DRECURSIVE_GENERATE:BOOL=ON
+cmake ..
 cd ..
