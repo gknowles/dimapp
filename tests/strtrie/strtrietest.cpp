f28f6b3102034d4e7db9e34a729c64f25a40e20c
gknowles <gknowles@ieee.org>
2022-01-03 14:34:54 -0800

    Update copyright

--- tests/strtrie/strtrietest.cpp
+++ tests/strtrie/strtrietest.cpp
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2019 - 2021.
+// Copyright Glen Knowles 2019 - 2022.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // strtrietest.cpp - dim test strtrie
