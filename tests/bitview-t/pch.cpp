44540586d5825ed15485dcc92ef96f5e83b53299
gknowles <gknowles@ieee.org>
2022-01-28 23:44:26 -0800

    Update copyright

--- tests/bitview-t/pch.cpp
+++ tests/bitview-t/pch.cpp
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2018.
+// Copyright Glen Knowles 2018 - 2022.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // pch.cpp - dim test bitview
--- tests/strtrie-t/pch.cpp
+++ tests/strtrie-t/pch.cpp
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2019.
+// Copyright Glen Knowles 2019 - 2022.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // pch.cpp - dim test strtrie
