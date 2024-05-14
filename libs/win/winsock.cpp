961d02308df3d61df3dc0e14ca62cd86cb1bb61b
gknowles <gknowles@ieee.org>
2024-05-13 22:54:20 -0700

    Render socket byte counters in SI units

--- libs/win/winsock.cpp
+++ libs/win/winsock.cpp
@@ -63,10 +63,22 @@ static RIO_EXTENSION_FUNCTION_TABLE s_rio;
 
 static atomic_int s_numSockets;
 
-static auto & s_perfReadTotal = uperf("sock.read bytes (total)");
-static auto & s_perfIncomplete = uperf("sock.write bytes (incomplete)");
-static auto & s_perfWaiting = uperf("sock.write bytes (waiting)");
-static auto & s_perfWriteTotal = uperf("sock.write bytes (total)");
+static auto & s_perfReadTotal = uperf(
+    "sock.read bytes (total)",
+    PerfFormat::kSiUnits
+);
+static auto & s_perfIncomplete = uperf(
+    "sock.write bytes (incomplete)",
+    PerfFormat::kSiUnits
+);
+static auto & s_perfWaiting = uperf(
+    "sock.write bytes (waiting)",
+    PerfFormat::kSiUnits
+);
+static auto & s_perfWriteTotal = uperf(
+    "sock.write bytes (total)",
+    PerfFormat::kSiUnits
+);
 
 // The data in the send queue is either too massive or too old.
 static auto & s_perfBacklog = uperf("sock.disconnect (write backlog)");
