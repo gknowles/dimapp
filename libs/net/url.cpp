6887cd5e93c83e0d39b5cac35250c2c43e2f1f3c
gknowles <gknowles@ieee.org>
2024-06-09 14:33:42 -0700

    Update copyright

--- libs/net/url.cpp
+++ libs/net/url.cpp
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2018 - 2021.
+// Copyright Glen Knowles 2018 - 2024.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // url.cpp - dim net
--- libs/win/windebug.cpp
+++ libs/win/windebug.cpp
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2020 - 2022.
+// Copyright Glen Knowles 2020 - 2024.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // windebug.cpp - dim windows platform
