d108fb4c861c0af4088198277bc20241615b667e
gknowles <gknowles@ieee.org>
2025-01-09 12:02:03 -0800

    Update dimcli

--- libs/cppconf/dimcli_userconfig.h
+++ libs/cppconf/dimcli_userconfig.h
@@ -9,10 +9,4 @@
 #define DIMCLI_LIB_KEEP_MACROS
 
 // Disable these warnings
-// Warning 5262 - disabled because [[fallthrough]] isn't supported by vs2015,
-// which is supported by dimcli.
-#pragma warning(disable: \
-    5262 /* implicit fall-through occurs here; are you missing a break
-            statement? Use [[fallthrough]] when a break statement is
-            intentionally omitted between cases */ \
-)
+// - none
