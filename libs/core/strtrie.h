dfb786e5906f36e0f495802219a399b5ec4b37eb
gknowles <gknowles@ieee.org>
2025-04-09 12:47:48 -0700

    Update copyright

--- libs/core/strtrie.h
+++ libs/core/strtrie.h
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2019 - 2024.
+// Copyright Glen Knowles 2019 - 2025.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // Set of strings, all non-const functions invalidate all iterators.
