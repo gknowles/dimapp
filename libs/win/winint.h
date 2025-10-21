6812646117587e4c876118fca5bf8b43db76455a
gknowles <gknowles@ieee.org>
2025-10-20 17:29:38 -0700

    Add <system_error> include for std::error_code.

--- libs/win/winint.h
+++ libs/win/winint.h
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2015 - 2022.
+// Copyright Glen Knowles 2015 - 2025.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // winint.h - dim windows platform
@@ -11,6 +11,7 @@
 #include <functional>
 #include <iostream>
 #include <memory> // pointer_traits
+#include <system_error>
 #include <type_traits>
 
 namespace Dim {
