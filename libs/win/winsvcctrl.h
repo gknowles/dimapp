a76c42d4d730b5af7528b306488929a64a05cd8f
gknowles <gknowles@ieee.org>
2025-10-20 18:07:38 -0700

    Update copyright

--- libs/win/winsvcctrl.h
+++ libs/win/winsvcctrl.h
@@ -1,4 +1,4 @@
-// Copyright Glen Knowles 2017 - 2022.
+// Copyright Glen Knowles 2017 - 2025.
 // Distributed under the Boost Software License, Version 1.0.
 //
 // winsvcctrl.h - dim windows platform
